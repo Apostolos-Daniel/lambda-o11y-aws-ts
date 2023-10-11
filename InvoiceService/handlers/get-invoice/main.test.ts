b9bba5edee984a1c9a7bedfdb61ab9adc2880712
Apostolis Apostolidis <github@toli.io>
2023-10-12 00:32:01 +0100

    add tests for lambda handler

---
+++ InvoiceService/handlers/get-invoice/main.test.ts
@@ -0,0 +1,31 @@
+import { handler } from "./main";
+import { Context } from "aws-lambda";
+
+describe("get-invoice handler", () => {
+
+it("returns a 200 status code", async () => {
+    const event = {};
+    const context: Context = {} as Context;
+    const callback = null;
+    const response = await handler(event, context, () => callback);
+    expect(response.statusCode).toEqual(200);
+});
+
+it("returns a body with a 'Hello' property", async () => {
+    const event = {};
+    const context: Context = {} as Context;
+    const callback = null;
+    const response = await handler(event, context, () => callback);
+    const body = JSON.parse(response.body);
+    expect(body).toHaveProperty("Hello");
+});
+
+it("returns a body with a 'Hello' property containing 'Invoice'", async () => {
+    const event = {};
+    const context: Context = {} as Context;
+    const callback = null;
+    const response = await handler(event, context, () => callback);
+    const body = JSON.parse(response.body);
+    expect(body.Hello).toContain("Invoice 💸");
+});
+});
