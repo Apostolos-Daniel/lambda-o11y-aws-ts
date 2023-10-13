fec0536debc3e9d43f2a9cca261165d14fc59d22
Apostolis Apostolidis <github@toli.io>
2023-10-13 02:15:57 +0100

    test

--- src/handlers/get-invoice/main.ts
+++ src/handlers/get-invoice/main.ts
@@ -4,7 +4,7 @@ import { setTag } from "../../helpers";
 export const handler: Handler = async (event) => {
   console.log("EVENT: \n", JSON.stringify(event, null, 2));
 
-  // set a datadog span custom tag test
+  // set a datadog span custom tag
   setTag("invoice_id", "some_value");
 
   const response = {
